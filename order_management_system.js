f9039e89ed6364c515a166789cc4e21c5686c47b
Christian Schmidt <cjschmidt33@gmail.com>
2024-09-17 16:44:59 -0400

    Initialize Orders Array

--- order_management_system.js
+++ order_management_system.js
@@ -6,3 +6,26 @@ const inventory = [
     {name: 'Jersey', price: 80, quantity: 20},
     {name: 'Hoop', price: 300, quantity: 5}
 ];
+
+
+// Task 2: Create an Orders Array of Order Objects
+
+const orders = [
+    {
+        customerName: 'Christian', 
+        items: [ {name: 'Basketball', quantity: 3}, {name: 'Shoes', quantity: 1} ],
+        orderStatus: 'Completed'
+    },
+    {
+        customerName: 'LeBron',
+        items: [ {name: 'Hoop', quantity: 1}, {name: 'Jersey', quantity: 2} ],
+        orderStatus: 'Pending'
+    },
+    {
+        customerName: 'Cade',
+        items: [ {name: 'Jersey', quantity: 3}, {name: 'Basketball', quantity: 1} ],
+        orderStatus: 'Pending'
+    }
+];
+
+console.log(orders)
