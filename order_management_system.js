fe1bd46ea08ad051f3c74c66040c058a1569c19c
Christian Schmidt <cjschmidt33@gmail.com>
2024-09-17 16:24:11 -0400

    Initialize Inventory with Product Objects

---
+++ order_management_system.js
@@ -0,0 +1,8 @@
+// Task 1: Create an Inventory Array of Product Objects
+
+const inventory = [
+    {name: 'Basketball', price: 60, quantity: 30},
+    {name: 'Shoes', price: 120, quantity: 10},
+    {name: 'Jersey', price: 80, quantity: 20},
+    {name: 'Hoop', price: 300, quantity: 5}
+];
